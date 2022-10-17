062dab37b8f6b4a291c19f5e38698a21058f8d0e
kramff <kramff@gmail.com>
2022-10-16 23:55:01 -0400

    movement, starting on tables

--- game.js
+++ game.js
@@ -1,25 +1,42 @@
 "use strict";
 console.log("alchemist game");
 
+// Main render stuff
 let scene;
 let camera;
 let renderer;
 
+// Specific render stuff
 let cubeGeometry;
 let playerMaterial;
-let playerObject;
+let playerMesh;
 
 let planeGeometry;
 let floorMaterial;
-let floorObject;
+let floorMesh;
+
+let tableMaterial;
+let tableMesh;
+let tableMaterial2;
+let tableMesh2;
+
+let sphereGeometry;
+let itemMaterial;
+let itemMesh;
 
 let sceneLight;
 let sceneLight2;
+
+// Game stuff
+let playerObject; 
 
 let wDown = false;
 let aDown = false;
 let sDown = false;
 let dDown = false;
+let oDown = false;
+let pDown = false;
+let spaceDown = false;
 
 let init = () => {
 	scene = new THREE.Scene();
@@ -30,22 +47,48 @@ let init = () => {
 
 	cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 	playerMaterial = new THREE.MeshToonMaterial({color: 0x22ff22});
-	playerObject = new THREE.Mesh(cubeGeometry, playerMaterial);
-	scene.add(playerObject);
+	playerMesh = new THREE.Mesh(cubeGeometry, playerMaterial);
+	scene.add(playerMesh);
 	camera.position.z = 10;
 
 	planeGeometry = new THREE.PlaneGeometry(10, 10);
 	floorMaterial = new THREE.MeshToonMaterial({color: 0x504030});
-	floorObject = new THREE.Mesh(planeGeometry, floorMaterial);
-	floorObject.position.set(0, 0, -0.5);
-	scene.add(floorObject);
+	floorMesh = new THREE.Mesh(planeGeometry, floorMaterial);
+	floorMesh.position.set(0, 0, -0.5);
+	scene.add(floorMesh);
 
-	sceneLight = new THREE.PointLight(0xffaaaa, 1, 10);
+	tableMaterial = new THREE.MeshToonMaterial({color: 0xccaa22});
+	tableMesh = new THREE.Mesh(cubeGeometry, tableMaterial);
+	tableMesh.position.set(3, 3, 0);
+	scene.add(tableMesh);
+
+	tableMaterial2 = new THREE.MeshToonMaterial({color: 0xccaa22});
+	tableMesh2 = new THREE.Mesh(cubeGeometry, tableMaterial2);
+	tableMesh2.position.set(3, -3, 0);
+	scene.add(tableMesh2);
+
+	sphereGeometry = new THREE.SphereGeometry(0.25, 6, 6);
+	itemMaterial = new THREE.MeshToonMaterial({color: 0x2266dd});
+	itemMesh = new THREE.Mesh(sphereGeometry, itemMaterial);
+	itemMesh.position.set(3, 3, 1);
+	scene.add(itemMesh);
+
+	sceneLight = new THREE.PointLight(0xff9999, 0.8, 14);
 	sceneLight.position.set(4, 4, 4);
 	scene.add(sceneLight);
 
-	sceneLight2 = new THREE.AmbientLight(0xffffff, 0.3);
-	//scene.add(sceneLight2);
+	sceneLight2 = new THREE.AmbientLight(0xffdddd, 0.4);
+	scene.add(sceneLight2);
+
+	playerObject = {
+		xSpeed: 0,
+		ySpeed: 0,
+		xPosition: 0,
+		yPosition: 0,
+		rotation: 0,
+		releasedGrab: true,
+		holdingItem: false,
+	}
 
 	addEventListener("keydown", keyDownFunction);
 	addEventListener("keyup", keyUpFunction);
@@ -66,25 +109,127 @@ let gameLoop = () => {
 	if (timeAccumulator > frameTime) {
 		gameLogic();
 	}
-	renderer.render(scene, camera);
+	renderFrame();
 	requestAnimationFrame(gameLoop);
 }
 
-let gameLogic = () => {
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
+let renderFrame = () => {
+	playerMesh.position.x = playerObject.xPosition;
+	playerMesh.position.y = playerObject.yPosition;
+	playerMesh.rotation.z = playerObject.rotation;
+	if (playerObject.holdingItem) {
+		itemMesh.parent = playerMesh;
+		itemMesh.position.set(1, 0, 0);
+	}
+	else {
+		itemMesh.parent = tableMesh;
+		itemMesh.position.set(0, 0, 1);
+	}
+	renderer.render(scene, camera);
+}
 
+
+let gameLogic = () => {
+
+	// Player Movement
+
+	let xSpeedChange = 0;
+	let ySpeedChange = 0;
 	if (wDown) {
-		playerObject.position.y += 0.1;
+		ySpeedChange += 0.02;
 	}
 	if (aDown) {
-		playerObject.position.x -= 0.1;
+		xSpeedChange -= 0.02;
 	}
 	if (sDown) {
-		playerObject.position.y -= 0.1;
+		ySpeedChange -= 0.02;
 	}
 	if (dDown) {
-		playerObject.position.x += 0.1;
+		xSpeedChange += 0.02;
+	}
+	// Diagonal movement
+	if (xSpeedChange !== 0 && ySpeedChange !== 0) {
+		xSpeedChange /= Math.SQRT2;
+		ySpeedChange /= Math.SQRT2;
+	}
+	let anyDirectionPressed = (xSpeedChange !== 0 || ySpeedChange !== 0);
+	let rotationChange = 0;
+	let targetRotation = Math.atan2(ySpeedChange, xSpeedChange);
+	let oppositeRotation = false;
+	if (anyDirectionPressed) {
+		if (playerObject.rotation !== targetRotation) {
+			var targetRotationDifference = Math.abs(playerObject.rotation - targetRotation);
+			// Apply spin to player's rotation toward targetRotation
+			if (playerObject.rotation > targetRotation) {
+				rotationChange -= 0.23;
+			}
+			else {
+				rotationChange += 0.23;
+			}
+			// If the target rotation difference is greater than pi, spin the opposite way
+			if (targetRotationDifference > Math.PI) {
+				rotationChange *= -1;
+				oppositeRotation = true;
+			}
+		}
+	}
+	let previousRotation = playerObject.rotation;
+	if (rotationChange !== 0) {
+		// Apply rotation
+		playerObject.rotation += rotationChange;
+		// Don't overshoot the targetRotation
+		if (oppositeRotation) {
+			if ((rotationChange > 0 && playerObject.rotation < targetRotation) || (rotationChange < 0 && playerObject.rotation > targetRotation)) {
+				playerObject.rotation = targetRotation; 
+			}
+		}
+		else {
+			if ((rotationChange > 0 && playerObject.rotation > targetRotation) || (rotationChange < 0 && playerObject.rotation < targetRotation)) {
+				playerObject.rotation = targetRotation; 
+			}
+		}
+		// Loop around the pi to negative pi limit
+		if (playerObject.rotation > Math.PI) {
+			playerObject.rotation -= Math.PI * 2;
+			previousRotation -= Math.PI * 2;
+		}
+		if (playerObject.rotation < -Math.PI) {
+			playerObject.rotation += Math.PI * 2;
+			previousRotation += Math.PI * 2;
+		}
+	}
+	// If turning, slow down movement
+	let rotationDifference = Math.abs(previousRotation - playerObject.rotation);
+	if (rotationDifference > 0.01) {
+		xSpeedChange *= 0.25;
+		ySpeedChange *= 0.25;
+	}
+	// Don't move while holding space
+	if (!spaceDown) {
+		playerObject.xSpeed += xSpeedChange;
+		playerObject.ySpeed += ySpeedChange;
+	}
+	playerObject.xPosition += playerObject.xSpeed;
+	playerObject.yPosition += playerObject.ySpeed;
+	playerObject.xSpeed *= 0.8;
+	playerObject.ySpeed *= 0.8;
+	// Apply more friction if stopping
+	if (!anyDirectionPressed || spaceDown) {
+		playerObject.xSpeed *= 0.9;
+		playerObject.ySpeed *= 0.9;
+	}
+
+	// World Interaction
+
+	if (pDown) {
+		if (playerObject.releasedGrab) {
+			// Grab input: try to grab or put down an item
+			playerObject.holdingItem = !playerObject.holdingItem;
+		}
+		playerObject.releasedGrab = false;
+	}
+	else {
+		playerObject.releasedGrab = true;
 	}
 }
 
@@ -101,6 +246,15 @@ let keyDownFunction = (event) => {
 	if (event.keyCode === 68) {
 		dDown = true;
 	}
+	if (event.keyCode === 79) {
+		oDown = true;
+	}
+	if (event.keyCode === 80) {
+		pDown = true;
+	}
+	if (event.keyCode === 32) {
+		spaceDown = true;
+	}
 }
 
 let keyUpFunction = (event) => {
@@ -116,7 +270,18 @@ let keyUpFunction = (event) => {
 	if (event.keyCode === 68) {
 		dDown = false;
 	}
+	if (event.keyCode === 79) {
+		oDown = false;
+	}
+	if (event.keyCode === 80) {
+		pDown = false;
+	}
+	if (event.keyCode === 32) {
+		spaceDown = false;
+	}
 }
 let resizeFunction = (event) => {
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 }
